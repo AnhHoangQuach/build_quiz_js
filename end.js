217937cae1ef23128b238fbf2a5f4aaf82b2ae73
AnhHoangQuach <hoanganh36.work@gmail.com>
2020-05-30 22:50:14 +0700

    display high score save in localstorage

--- end.js
+++ end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentStore = localStorage.getItem('mostRecentScore');
 
+const MAX_HIGH_SCORES = 5;
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 finalScore.innerText = mostRecentStore;
 
 username.addEventListener('keyup', () => {
@@ -11,4 +15,17 @@ username.addEventListener('keyup', () => {
 saveHighScore = e => {
     console.log("clicked the save button");
     e.preventDefault();
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    }
+
+    highScores.push(score);
+
+    highScores.sort((a,b) =>  b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign("index.html");
 };
