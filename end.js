56d81262c0300af132c16a3faa5b66010cb16dd1
AnhHoangQuach <hoanganh36.work@gmail.com>
2020-05-30 22:04:52 +0700

    create and style end page

---
+++ end.js
@@ -0,0 +1,14 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentStore = localStorage.getItem('mostRecentScore');
+
+finalScore.innerText = mostRecentStore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+saveHighScore = e => {
+    console.log("clicked the save button");
+    e.preventDefault();
+};
